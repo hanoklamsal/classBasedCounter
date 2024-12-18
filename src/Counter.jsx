32fe45a0bc981192960bf482da408fd07e8bcae0
Hanok Lamsal <hanoklamsal@gmail.com>
2024-12-18 12:16:51 +0530

    counter added

---
+++ src/Counter.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+export default class Counter extends Component {
+
+  state = {
+    count: 0,
+  };
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div style={{padding:'16px'}}>
+        <h1>Counter</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
